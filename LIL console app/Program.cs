31d0b37d8a9716309f8669bbd6a4d7534e2d82ce
CSimoneCode <csimonecode@gmail.com>
2021-03-04 12:07:37 -0700

    initial commit

---
+++ LIL console app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIL_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to C# Essential Training part 1");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            // How to comment and leave documentation
+
+
+        }
+    }
+}
